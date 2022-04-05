03e4b7fd2d03a9c11a572dd5f1452aeb9dea4cda
hehe-wang <77651819+hehe-wang@users.noreply.github.com>
2022-04-05 16:25:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,8 +12,12 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-sed -i '/TARGET_DEVICES += jcg_y2/a\\ndefine Device\/jdcloud_re-sp-01b\n  \$(Device\/dsa-migration)\n  \$(Device\/uimage-lzma-loader)\n  IMAGE_SIZE := 27328k\n  DEVICE_VENDOR := JDCloud\n  DEVICE_MODEL := RE-SP-01B\n  DEVICE_PACKAGES := kmod-fs-ext4 kmod-mt7603 kmod-mt7615e kmod-sdhci-mt7620 kmod-usb3 wpad-openssl\nendef\nTARGET_DEVICES += jdcloud_re-sp-01b' target/linux/ramips/image/mt7621.mk
+sed -i '/TARGET_DEVICES += jcg_y2/a\\ndefine Device\/jdcloud_re-sp-01b\n  SOC :=mt7621  IMAGE_SIZE := 27328k\n  DEVICE_VENDOR := JDCloud\n  DEVICE_MODEL := RE-SP-01B\n  DEVICE_PACKAGES := kmod-fs-ext4 kmod-mt7603 kmod-mt7615e kmod-sdhci-mt7620 kmod-usb3 wpad-openssl\nendef\nTARGET_DEVICES += jdcloud_re-sp-01b' target/linux/ramips/image/mt7621.mk
 
 sed -i '/lenovo,newifi-d1|\/i\        jdcloud,re-sp-01b|\\' target/linux/ramips/mt7621/base-files/etc/board.d/02_network
+
+sed -i '/mikrotik,routerboard-750gr3/i\        jdcloud,re-sp-01b)\n                local index="\$(find_mtd_index "config")"\n                label_mac=\$(macaddr_canonicalize \$(dd if="/dev/mtd\${index}" bs=12 skip=17449 iflag=skip_bytes count=1 2>/dev/null))\n                lan_mac=\$label_mac\n                wan_mac=\$label_mac\n                ;;' target/linux/ramips/mt7621/base-files/etc/board.d/02_network 
+
+sed -i '/linksys,e5600|\/i\        jdcloud,re-sp-01b)\n                local index="\$(find_mtd_index "config")"\n                local label_mac=\$(macaddr_canonicalize \$(dd if="/dev/mtd\${index}" bs=12 skip=17449 iflag=skip_bytes count=1 2>/dev/null))\n                [ "\$PHYNBR" -eq 0 ] && echo \$label_mac > /sys\${DEVPATH}/macaddress\n                [ "\$PHYNBR" -eq 1 ] && echo macaddr_add \$label_mac 0x800000 > /sys\${DEVPATH}/macaddress\n                ;;' target/linux/ramips/mt7621/base-files/etc/hotplug.d/ieee80211/10_fix_wifi_mac
 
 sed -i 's/set wireless.radio${devidx}.disabled=1/set wireless.radio${devidx}.disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
